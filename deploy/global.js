36e930b7a60427bf9a909ddee65492bde16e2733
Denys <denys.afendikov@axel.org>
2023-02-15 01:19:19 +0200

    update

--- deploy/global.js
+++ deploy/global.js
@@ -1,20 +1,25 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const {REST} = require("@discordjs/rest");
+const {Routes} = require("discord-api-types/v9");
 const getConfig = require("../util/getConfig");
 const LoadCommands = require("../util/loadCommands");
 
 (async () => {
-	const config = await getConfig();
-	const rest = new REST({ version: "9" }).setToken(config.token);
-	const commands = await LoadCommands().then((cmds) => {
-		return [].concat(cmds.slash).concat(cmds.context);
-	});
+    const config = await getConfig();
+    const rest = new REST({version: "9"}).setToken(config.token);
+    const commands = await LoadCommands().then((cmds) => {
+        return [].concat(cmds.slash).concat(cmds.context);
+    });
 
-	console.log("Deploying commands to global...");
-	await rest
-		.put(Routes.applicationCommands(config.clientId), {
-			body: commands,
-		})
-		.catch(console.log);
-	console.log("Successfully deployed commands!");
-})().then(() => process.exit(0));
+    console.log("Deploying commands to global...");
+    await rest
+        .put(Routes.applicationCommands(config.clientId), {
+            body: commands,
+        })
+        .catch(console.log);
+    console.log("Successfully deployed commands!");
+    process.exit(0);
+})()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    });
